4e689a1aa18c43e44bb9f711a35b2401f3a30b31
doofmars <jan-preuss@gmx.de>
2019-06-20 23:56:52 +0200

    Mosaic generation prototype
    still issues with memory consumption
    Removed old test code
    Write rows and columns for target video

--- mosaic.py
+++ mosaic.py
@@ -1,33 +1,45 @@
 from moviepy.editor import *
 import os
 
-directory = os.fsencode('source/Minifigures')
+directory_string = 'source'
+
+directory = os.fsencode(directory_string)
+
+start = 0
+stop = 1
+videoX = 25
+videoY = 14
+xDimension = 640
+yDimension = 360
+# Data holder
+videos = []
+x = 0
+y = 0
+
+if not os.path.exists("source/loaded"):
+    os.system('youtube-dl -o "source/%(upload_date)s_%(id)s.%(ext)s" -f worst https://www.youtube.com/user/HeldderSteine')
+    open('source/loaded', 'a').close()
+else:
+	print('source files already downloaded')
 
 for file in os.listdir(directory):
      filename = os.fsdecode(file)
      if filename.endswith(".mp4"): 
-         print(filename)
-         continue
+         print("%s/%s (%i,%i)" % (directory_string, filename, x, y))
+         video = VideoFileClip(directory_string + "/" + filename).subclip(start, stop).set_position((x * xDimension, y * yDimension))
+         videos.append(video)
+         if x < videoX:
+         	x = x + 1
+         else: 
+         	x = 0
+         	y = y + 1
+         	if y >= videoY:
+         		print("reached end of area")
+         		break
      else:
          continue
-
-
-start = 0
-stop = 1
-
-video0 = VideoFileClip( 'source/Minifigures/20141004_QwvV2zfCL68.mp4').subclip(start, stop)
-video1 = VideoFileClip( 'source/Minifigures/20150903_syT1qPR_nYI.mp4').subclip(start, stop)
-video2 = VideoFileClip( 'source/Minifigures/20170517_-OsKQIMf0ls.mp4').subclip(start, stop)
-video3 = VideoFileClip( 'source/Minifigures/20170517_DnCd7KEJNmU.mp4').subclip(start, stop)
-video4 = VideoFileClip( 'source/Minifigures/20170806_MXXPJ-oJlTY.mp4').subclip(start, stop)
-video5 = VideoFileClip( 'source/Minifigures/20180112_ySMTFt9rftg.mp4').subclip(start, stop)
-video6 = VideoFileClip( 'source/Minifigures/20180417_XUbxc-Qt1a8.mp4').subclip(start, stop)
-video7 = VideoFileClip( 'source/Minifigures/20180928_Ss69gFLHNqQ.mp4').subclip(start, stop)
-video8 = VideoFileClip( 'source/Minifigures/20190202_ldz23LoNhQo.mp4').subclip(start, stop)
-video9 = VideoFileClip( 'source/Minifigures/20190414_qYLp4K4LpdA.mp4').subclip(start, stop)
-video10 = VideoFileClip('source/Minifigures/20190504_bJCIbEw-JGk.mp4').subclip(start, stop)
 
 #final_clip = clips_array([[video0, video1, video2, video3], [video4, video5, video6, video7]])
 #final_clip.resize(width=1920).write_videofile("my_stack.mp4")
 
-CompositeVideoClip([video0, video1.set_position((640, 0)), video2.set_position((0, 360))], size=(1280,720)).write_videofile("my_stack.mp4")
+CompositeVideoClip(videos, size=(xDimension*(videoX + 1),yDimension*(videoY + 1))).resize(width=1920).write_videofile("my_stack.mp4")
