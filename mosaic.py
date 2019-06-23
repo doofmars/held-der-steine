31b4f337b540940c251e291e546020130897b7c8
doofmars@switch <jan-preuss@gmx.de>
2019-06-23 11:42:30 +0200

    Add comments

--- mosaic.py
+++ mosaic.py
@@ -1,14 +1,19 @@
 from moviepy.editor import *
 import subprocess, os, platform
 
+#Youtuber target url
 youtube_url = 'https://www.youtube.com/user/HeldderSteine'
 
+#File pahts, flags and output
 source_dir = 'source'
+work_dir = 'work'
 loaded_flag = 'loaded'
 output = 'my_stack.mp4'
 
+#Length of all clips
 start = 0
 stop = 10
+#Number of videos in mosaic
 videoX = 10
 videoY = 10
 #640x360
@@ -19,12 +24,14 @@ videos = []
 x = 0
 y = 0
 
+#Start downloading using youtube-dl
 if not os.path.exists(source_dir + '/' + loaded_flag):
     open(source_dir + '/' + loaded_flag, 'a').close()
     os.system('youtube-dl -o "'+source_dir+'/%(upload_date)s_%(id)s.%(ext)s" -f worst ' + youtube_url)
 else:
-	print('source files already downloaded')
+	print('Source files already downloaded')
 
+#Iterate over files, position and put into holding array
 for file in sorted(os.listdir(source_dir), reverse=True):
      filename = os.fsdecode(file)
      if filename.endswith(".mp4"): 
@@ -45,6 +52,7 @@ for file in sorted(os.listdir(source_dir), reverse=True):
 #final_clip = clips_array([[video0, video1, video2, video3], [video4, video5, video6, video7]])
 #final_clip.resize(width=1920).write_videofile("my_stack.mp4")
 
+#Composite clips and render out
 CompositeVideoClip(videos, size=(xDimension*(videoX),yDimension*(videoY))).resize(width=1920).write_videofile(output)
 
 # Finally open rendered video
